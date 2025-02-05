211f54ef35206d33e6bd558a381a9b41e8a2b93a
Spatial Continuum <spatialcontinuum@gmail.com>
2025-02-05 23:01:56 +0530

    Update globalImport.js
    updated production url

--- src/globalImport.js
+++ src/globalImport.js
@@ -1,6 +1,6 @@
 const API_URL1={ 
     development: 'https://service.pintogroups.in',
-    production: 'http://139.5.189.164'
+    production: 'https://service.pintogroups.in'
   }
   const NODE_ENV= import.meta.env.MODE || 'development' 
   const API_URL = API_URL1[NODE_ENV];
